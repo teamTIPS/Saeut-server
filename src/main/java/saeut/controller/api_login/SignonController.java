0771063777c1d540aaa1caef69e33c5585a14bec
angelatto <pudri109@gmail.com>
2020-08-05 22:40:46 +0900

    fix : 주석 수정

--- src/main/java/saeut/controller/api_login/SignonController.java
+++ src/main/java/saeut/controller/api_login/SignonController.java
@@ -1,18 +1,13 @@
 package saeut.controller.api_login;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import saeut.domain.Account;
-import saeut.domain.Certificate;
 import saeut.service.facade.MyPageFacade;
 
 @RestController

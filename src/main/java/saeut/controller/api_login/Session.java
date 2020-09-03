ab823ec1d787573c46810bdaa09601d965a5985d
angelatto <pudri109@gmail.com>
2020-09-03 18:23:18 +0900

    add : Session

--- src/main/java/saeut/controller/api_login/Session.java
+++ src/main/java/saeut/controller/api_login/Session.java
@@ -3,9 +3,11 @@ package saeut.controller.api_login;
 import java.util.Date;
 
 public class Session {
-	
+	// 처음에 로그인할때 토큰 발급 
+	// 너로 로그인 만료되면 리프레시로 발급해달라고 요청 
+	// 두개를 보내면 새로 발급된걸 리턴해주는 형태로 .. 
 	private String account_id;
-	private String accessToken;
+	private String accessToken; 
 	private String refreshToken;
 	private Date accessToken_expireTime;
 	private Date refreshToken_expireTime;

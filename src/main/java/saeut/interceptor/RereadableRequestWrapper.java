426dfefb275f8dc1d6ca78bc216b3f720145f74a
CHAE JEONG LEE <56299764+angelatto@users.noreply.github.com>
2020-09-13 22:57:09 +0900

    add : RereadableRequestWrapper.java

---
+++ src/main/java/saeut/interceptor/RereadableRequestWrapper.java
@@ -0,0 +1,85 @@
+package saeut.interceptor;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.util.StringUtils;
+
+
+public class RereadableRequestWrapper extends HttpServletRequestWrapper {
+	// 일단 이 코드는 서버에서 POST방식의 "application/json" 타입만 받는다 문제없음 
+	//  아직 Json GET요청은 처리하지 않은 상태 
+	// 개설할 사항 : Spring AOP 방식 사용하기 
+	private final Charset encoding;
+    private byte[] rawData;
+
+    public RereadableRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+
+        String characterEncoding = request.getCharacterEncoding();
+        if (StringUtils.isEmpty(characterEncoding)) {
+            characterEncoding = StandardCharsets.UTF_8.name();
+        }
+        this.encoding = Charset.forName(characterEncoding);
+
+        // Convert InputStream data to byte array and store it to this wrapper instance.
+        try {
+            InputStream inputStream = request.getInputStream();
+            this.rawData = IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.rawData);
+        ServletInputStream servletInputStream = new ServletInputStream() {
+            public int read() throws IOException {
+                return byteArrayInputStream.read();
+            }
+
+			@Override
+			public boolean isFinished() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public boolean isReady() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public void setReadListener(ReadListener readListener) {
+				// TODO Auto-generated method stub
+				
+			}
+        };
+        return servletInputStream;
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream(), this.encoding));
+    }
+
+    @Override
+    public ServletRequest getRequest() {
+        return super.getRequest();
+    }
+}
+	

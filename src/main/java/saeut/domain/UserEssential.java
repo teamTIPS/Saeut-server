48db5e3ffff1927aa9b9ac02f80635ca48ae934f
leejieuns2 <2jjingni@gmail.com>
2020-10-24 03:09:47 +0900

    hotfix : jsonformat android와의 불일치 해결

--- src/main/java/saeut/domain/UserEssential.java
+++ src/main/java/saeut/domain/UserEssential.java
@@ -22,7 +22,7 @@ public class UserEssential implements Serializable {
     private String password;
     private String name;
     private int phone;
-    @JsonFormat(pattern="yyyy-MM-dd", timezone="Asia/Seoul")
+    @JsonFormat(pattern="yyyyMMdd", timezone="Asia/Seoul")
     private Date birth;
     private int gender;
     private boolean perm1;
